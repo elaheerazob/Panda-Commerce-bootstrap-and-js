c0da4aba530db89483b6d7b504796c4416a05cc8
elaheerazob <ridwanelahee282@gmail.com>
2022-02-09 12:29:07 +0600

    js add

---
+++ coustom.js
@@ -0,0 +1,13 @@
+const h1Color = document.getElementsByTagName('h1');
+for(const colorH1 of h1Color){
+    colorH1.style.color = 'green'
+}
+
+document.getElementById('backpack').style.backgroundColor = ('tomato ');
+
+
+const cards =document.getElementsByClassName('card');
+for(const card of cards){
+    card.style.borderRadius ='30px';
+}
+
