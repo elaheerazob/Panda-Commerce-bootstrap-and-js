abf1d8b029b4dcdd4d356c82d395460312f3787a
elaheerazob <ridwanelahee282@gmail.com>
2022-02-09 17:22:53 +0600

    add

--- coustom.js
+++ coustom.js
@@ -1,6 +1,6 @@
 const h1Color = document.getElementsByTagName('h1');
 for(const colorH1 of h1Color){
-    colorH1.style.color = 'green'
+    colorH1.style.color = 'lightblue '
 }
 
 document.getElementById('backpack').style.backgroundColor = ('tomato ');
@@ -11,3 +11,25 @@ for(const card of cards){
     card.style.borderRadius ='30px';
 }
 
+document.getElementById('shoeBtn1').addEventListener('click',function(){
+    console.log('borrrrr');
+})
+
+
+//Button Delete
+
+/* const allButton =document.getElementsByTagName('button');
+for(const buttons of allButton){
+    buttons.addEventListener('click',function(e){
+        e.target.parentNode.removeChild(e.target)
+    })
+} */
+
+document.getElementById('delete-conferm').addEventListener('keyup',function(e){
+    const deleteButton =document.getElementById('delete-btn');
+    if(e.target.value =='email'){
+        deleteButton.removeAttribute('disabled')
+    }else{
+        deleteButton.setAttribute('disabled',true)
+    }
+})
